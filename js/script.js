46f3ef2b3e60a43bef0797efe6d9a95a5d26fa90
Sukhrobjon [FE20] <126710684+LoganS03@users.noreply.github.com>
2023-07-06 17:42:03 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
-let hours = document.querySelector(".hours");
-let minutes = document.querySelector(".minutes");
-let seconds = document.querySelector(".seconds");
+let secondsPassed = document.querySelector(".seconds_passed")
 
-let setTime = document.querySelector("input")
-let button = document.querySelector("button")
+let hours = document.querySelector(".hours")
+let minutes = document.querySelector(".minutes")
+let seconds = document.querySelector(".seconds")
+let daytime = document.querySelector(".daytime")
+let copyButton = document.querySelector(".copy_btn")
 
-let yourTimeDiv = document.querySelector(".setted_time")
-let yourTime = document.querySelector(".your_time")
+let convertInput = document.querySelector(".timestamp input")
+let convertBtn = document.querySelector(".convert")
+let table = document.querySelector("table")
+let format = document.querySelector("table .format td")
+let GMT = document.querySelector("table .GMT td")
+let timeZone = document.querySelector("table .time_zone td")
+let relative = document.querySelector("table .relative td")
 
 function clock(){
-    let time = new Date;
+    let now = new Date()
+    
+    secondsPassed.innerText = Math.trunc(now.getTime() / 1000)
 
-    button.addEventListener("click", () => {
-        yourTimeDiv.style.display = "block"
-        yourTime.innerText = addZero(newClock(setTime.value, time.getHours())) + ":" + addZero(time.getMinutes()) + ":" + addZero(time.getSeconds())
-    })
-
-    hours.innerText = addZero(time.getHours())
-    minutes.innerText = addZero(time.getMinutes())
-    seconds.innerText = addZero(time.getSeconds())
+    minutes.innerText = addZero(now.getMinutes())
+    seconds.innerText = addZero(now.getSeconds())
+    if(now.getHours() >= 12){
+        hours.innerText = now.getHours() - 12
+        daytime.innerText = "PM"
+    }
+    else{
+        hours.innerText = now.getHours()
+        daytime.innerText = "AM"
+    }
 }
 
 function addZero(number){
@@ -30,17 +40,148 @@ function addZero(number){
     }
 }
 
-clock()
-setInterval(clock, 1000);
-
-function newClock(newTime, time){
-    newTime = newTime % 24
-    newTime = time + +setTime.value
-    newTime = newTime % 24
-
-    if(newTime < 0){
-        newTime += 24
+copyButton.addEventListener("click", (event) => {
+    if(event.copyboard){
+        copyButton.innerText = "Copied!"
+        setTimeout(() => {
+            copyButton.innerText = "Copy"
+        }, 1000);
+        event.copyboard.writeText(secondsPassed.innerText)
     }
+})
 
-    return newTime
-}
+convertBtn.addEventListener("click", () => {
+    if(convertInput.value != 0){
+        table.style.display = "block"
+        
+        let value = 0
+        if(convertInput.value.length < 12){
+            format.innerText = "Seconds"
+            value = convertInput.value * 1000
+        }
+        else if(convertInput.value.length < 15){
+            format.innerText = "Milliseconds (1/1,000 second)"
+            value = convertInput.value
+        }
+        else if(convertInput.value.length < 17){
+            format.innerText = "Microseconds (1/1,000,000 second)"
+            value = convertInput.value / 1000
+        }
+        else{
+            format.innerText = "Nanoseconds (1 billionth of a second)"
+            value = convertInput.value / 1000000
+        }
+        let then = new Date(+value)
+        let now = new Date()
+        let time = addZero(then.getUTCHours()) + ":" + addZero(then.getUTCMinutes()) + ":" + addZero(then.getUTCSeconds()) + " GMT+0000"
+        
+        GMT.innerText = then.toDateString() + " " + time
+        timeZone.innerText = then
+
+        if(then.getFullYear() > now.getFullYear()){
+            if(then.getFullYear() - now.getFullYear() == 1){
+                relative.innerText = "in a year"
+            }
+            else{
+                relative.innerText = "in " + (then.getFullYear() - now.getFullYear()) + " years"
+            }
+        }
+        else if(then.getFullYear() < now.getFullYear()){
+            if(now.getFullYear() - then.getFullYear() == 1){
+                relative.innerText = "a year ago"
+            }
+            else{
+                relative.innerText = now.getFullYear() - then.getFullYear() + " years ago"
+            }
+        }
+        else{
+            if(then.getMonth() > now.getMonth()){
+                if(then.getMonth() - now.getMonth() == 1){
+                    relative.innerText = "in a month"
+                }
+                else{
+                    relative.innerText = "in " + (then.getMonth() - now.getMonth()) + " months"
+                }
+            }
+            else if(then.getMonth() < now.getMonth()){
+                if(now.getMonth() - then.getMonth() == 1){
+                    relative.innerText = "a month ago"
+                }
+                else{
+                    relative.innerText = now.getMonth() - then.getMonth() + " months ago"
+                }
+            }
+            else{
+                if(then.getDay() > now.getDay()){
+                    if(then.getDay() - now.getDay() == 1){
+                        relative.innerText = "in a day"
+                    }
+                    else{
+                        relative.innerText = "in " + (then.getDay() - now.getDay()) + " days"
+                    }
+                }
+                else if(then.getDay() < now.getDay()){
+                    if(now.getDay() - then.getDay() == 1){
+                        relative.innerText = "a day ago"
+                    }
+                    else{
+                        relative.innerText = now.getDay() - then.getDay() + " days ago"
+                    }
+                }
+                else{
+                    if(then.getHours() > now.getHours()){
+                        if(then.getHours() - now.getHours() == 1){
+                            relative.innerText = "in a hour"
+                        }
+                        else{
+                            relative.innerText = "in " + (then.getHours() - now.getHours()) + " hours"
+                        }
+                    }
+                    else if(then.getHours() < now.getHours()){
+                        if(now.getHours() - then.getHours() == 1){
+                            relative.innerText = "a hour ago"
+                        }
+                        else{
+                            relative.innerText = now.getHours() - then.getHours() + " hours ago"
+                        }
+                    }
+                    else{
+                        if(then.getMinutes() > now.getMinutes()){
+                            if(then.getMinutes() - now.getMinutes() == 1){
+                                relative.innerText = "in a minute"
+                            }
+                            else{
+                                relative.innerText = "in " + (then.getMinutes() - now.getMinutes()) + " minutes"
+                            }
+                        }
+                        else if(then.getMinutes() < now.getMinutes()){
+                            if(now.getMinutes() - then.getMinutes() == 1){
+                                relative.innerText = "a minute ago"
+                            }
+                            else{
+                                relative.innerText = now.getMinutes() - then.getMinutes() + " minutes ago"
+                            }
+                        }
+                        else{
+                            if(then.getSeconds() > now.getSeconds()){
+                                relative.innerText = "in a few seconds"
+                            }
+                            else{
+                                relative.innerText = "a few seconds ago"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
+
+convertInput.addEventListener("keydown", (event) => {
+    if(event.key == "Enter"){
+        convertBtn.click();
+    }
+})
+
+clock()
+setInterval(clock, 1000)
